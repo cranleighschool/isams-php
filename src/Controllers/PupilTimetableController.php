feebe12cd6a5944d44503d14b15d50a1c10e660f
Simon Mitchell <spkm@cranleigh.org>
2021-06-18 14:27:43 +0100

    (feat) add the REST api timetable endpoint

---
+++ src/Controllers/PupilTimetableController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace spkm\isams\Controllers;
+
+use spkm\isams\Endpoint;
+use Illuminate\Support\Collection;
+use spkm\isams\Wrappers\PupilTimetable;
+
+class PupilTimetableController extends Endpoint
+{
+    /**
+     * Set the URL the request is made to.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    protected function setEndpoint()
+    {
+        $this->endpoint = $this->getDomain() . '/api/timetables/students';
+    }
+
+    /**
+     * Get the timetable for the specified pupil.
+     *
+     * @param string $schoolId
+     * @return \Illuminate\Support\Collection
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function show(string $schoolId): Collection
+    {
+        $this->endpoint = $this->endpoint . '/' . $schoolId;
+
+        $response = $this->guzzle->request('GET', $this->endpoint, ['headers' => $this->getHeaders()]);
+
+        $decoded = json_decode($response->getBody()->getContents());
+
+        return collect($decoded)->map(function ($item) {
+            return new PupilTimetable($item);
+        });
+    }
+}
---
+++ src/Wrappers/PupilTimetable.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace spkm\isams\Wrappers;
+
+use spkm\isams\Wrapper;
+
+/**
+ * Wrapper for the array returned by the iSAMS REST API endpoint.
+ */
+class PupilTimetable extends Wrapper
+{
+    /**
+     * Handle the data.
+     *
+     * @return void
+     */
+    protected function handle()
+    {
+        //
+    }
+}
