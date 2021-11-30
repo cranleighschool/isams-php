f49fca1fbc05014516af966f083723da0222d63d
Simon Mitchell <simon@mitchellmail.uk>
2021-11-30 14:42:52 +0000

    feat: add other school professions endpoint

---
+++ src/Wrappers/SchoolProfession.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace spkm\isams\Wrappers;
+
+use spkm\isams\Wrapper;
+
+/**
+ * Wrapper for the array returned by the iSAMS REST API endpoint.
+ */
+class SchoolProfession extends Wrapper
+{
+    /**
+     * Handle the data.
+     *
+     * @return void
+     */
+    protected function handle(): void
+    {
+        //
+    }
+}
