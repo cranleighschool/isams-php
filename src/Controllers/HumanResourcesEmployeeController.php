2cfba8db8ebe1d4293157cb87a69d32567b34d80
Simon Mitchell <simon@mitchellmail.uk>
2021-11-30 14:26:36 +0000

    refactor: move the employee photo endpoint to its own controller

--- src/Controllers/HumanResourcesEmployeeController.php
+++ src/Controllers/HumanResourcesEmployeeController.php
@@ -3,17 +3,12 @@
 namespace spkm\isams\Controllers;
 
 use Exception;
-use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Exception\RequestException;
+use spkm\isams\Endpoint;
+use spkm\isams\Wrappers\Employee;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Image;
-use Intervention\Image\ImageManagerStatic;
-use spkm\isams\Endpoint;
-use spkm\isams\Wrappers\Employee;
-use spkm\isams\Wrappers\EmployeePhoto;
+use GuzzleHttp\Exception\GuzzleException;
 
 class HumanResourcesEmployeeController extends Endpoint
 {
@@ -22,7 +17,7 @@ class HumanResourcesEmployeeController extends Endpoint
      *
      * @return void
      *
-     * @throws \Exception
+     * @throws Exception
      */
     protected function setEndpoint(): void
     {
@@ -57,7 +52,7 @@ class HumanResourcesEmployeeController extends Endpoint
         }
 
         if ($totalCount !== $items->count()) {
-            throw new \Exception($items->count() . ' items were returned instead of ' . $totalCount . ' as specified on page 1.');
+            throw new Exception($items->count() . ' items were returned instead of ' . $totalCount . ' as specified on page 1.');
         }
 
         $items = $this->sortBySurname($items);
@@ -121,54 +116,6 @@ class HumanResourcesEmployeeController extends Endpoint
         $decoded = json_decode($response->getBody()->getContents());
 
         return new Employee($decoded);
-    }
-
-    /**
-     * Gets the Current Photo for the Employee.
-     *
-     * @param  int  $id
-     * @param  int  $quality
-     * @return EmployeePhoto
-     *
-     * @throws GuzzleException
-     */
-    public function getCurrentPhoto(int $id, int $quality = 75): EmployeePhoto
-    {
-        /**
-         * At the moment this package doesn't auto-include Intervention, so we need to check for its existence first.
-         */
-        if (! method_exists(ImageManagerStatic::class, 'make')) {
-            throw new Exception('This method requires Intervention/Image package.', 500);
-        }
-
-        try {
-            /**
-             * Hello ISAMS!
-             */
-            $response = $this->guzzle->request('GET', $this->endpoint . '/' . $id . '/photos/current', ['headers' => $this->getHeaders()]);
-
-            /**
-             * Get the Image and Save it to Storage.
-             */
-            $image = ImageManagerStatic::make($response->getBody()->getContents());
-            $data = $image->encode('jpg', $quality);
-            $save = Storage::put($id . '.jpg', $data);
-
-            /**
-             * Grab the image out of storage and encode it as a Data URL
-             * Then Delete the image from Storage. (Like we'd never know it was there!).
-             */
-            $image = storage_path('app/' . $id . '.jpg');
-            $image = ImageManagerStatic::make($image)->encode('data-url');
-            Storage::delete($id . '.jpg');
-        } catch (RequestException $exception) {
-            $image = ['error' => json_decode($exception->getResponse()->getBody()->getContents())];
-        }
-
-        /**
-         * Return an instance of the EmployeePhoto class.
-         */
-        return new EmployeePhoto($image);
     }
 
     /**
