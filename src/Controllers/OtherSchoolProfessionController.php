547319fbf1288a4c10ec65aa82fb13cadc00cda8
Simon Mitchell <simon@mitchellmail.uk>
2021-11-30 14:42:37 +0000

    feat: add other school professions endpoint

---
+++ src/Controllers/OtherSchoolProfessionController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace spkm\isams\Controllers;
+
+use Exception;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
+use spkm\isams\Endpoint;
+use spkm\isams\Wrappers\SchoolProfession;
+
+class OtherSchoolProfessionController extends Endpoint
+{
+    /**
+     * Set the URL the request is made to.
+     *
+     * @return void
+     *
+     * @throws Exception
+     */
+    protected function setEndpoint(): void
+    {
+        $this->endpoint = $this->getDomain() . '/api/otherschools/professions';
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Collection
+     * @throws GuzzleException
+     */
+    public function index(): Collection
+    {
+        $key = $this->institution->getConfigName() . 'otherSchoolProfessions.index';
+
+        $response = $this->guzzle->request('GET', $this->endpoint, ['headers' => $this->getHeaders()]);
+
+        return Cache::remember($key, $this->getCacheDuration(), function () use ($response) {
+            return $this->wrapJson($response->getBody()->getContents(), 'professions', SchoolProfession::class);
+        });
+    }
+}
