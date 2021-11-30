f34b5e82d6c1c87170fbb462a2d114b500a0b630
Simon Mitchell <simon@mitchellmail.uk>
2021-11-30 14:47:13 +0000

    feat: add registration present codes endpoint

---
+++ src/Controllers/RegistrationPresentCodeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace spkm\isams\Controllers;
+
+use Exception;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Collection;
+use spkm\isams\Endpoint;
+
+class RegistrationPresentCodeController extends Endpoint
+{
+    /**
+     * @throws Exception
+     */
+    protected function setEndpoint(): void
+    {
+        $this->endpoint = $this->getDomain() . '/api/registration/presentcodes';
+    }
+
+    /**
+     * @return Collection
+     * @throws GuzzleException
+     */
+    public function index(): Collection
+    {
+        $response = $this->guzzle->request('GET', $this->endpoint, ['headers' => $this->getHeaders()]);
+
+        $absenceCodes = json_decode($response->getBody()->getContents())->presentCodes;
+
+        return collect($absenceCodes)->pluck('name');
+    }
+}
