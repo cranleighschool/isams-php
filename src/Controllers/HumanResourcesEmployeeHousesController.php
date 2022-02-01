1cbadab50ebecb716edfc53e3811e29c5f816fb0
Fred Bradley <code@fredbradley.co.uk>
2022-02-01 16:14:55 +0000

    Add endpoint for Employee/houses

---
+++ src/Controllers/HumanResourcesEmployeeHousesController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace spkm\isams\Controllers;
+
+use spkm\isams\Endpoint;
+use spkm\isams\Wrappers\EmployeeHouse;
+
+class HumanResourcesEmployeeHousesController extends Endpoint
+{
+    /**
+     * Set the URL the request is made to.
+     *
+     * @return void
+     *
+     * @throws \Exception
+     */
+    protected function setEndpoint(): void
+    {
+        $this->endpoint = $this->getDomain().'/api/humanresources/employees';
+    }
+
+    /**
+     * Show the specified resource.
+     *
+     * @param  int  $id
+     * @return \spkm\isams\Wrappers\Employee
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function show(int $id)
+    {
+        $response = $this
+            ->guzzle
+            ->request(
+                'GET',
+                $this->endpoint.'/'.$id.'/houses',
+                [
+                    'headers' => $this->getHeaders(),
+                ]
+            );
+
+        $decoded = json_decode($response->getBody()->getContents());
+        return new EmployeeHouse($decoded, $this->institution);
+    }
+}
