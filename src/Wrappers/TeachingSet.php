4d312f1313a733d7f8396a3489c672fd1849879d
Simon Mitchell <simon@mitchellmail.uk>
2022-09-16 14:34:09 +0100

    feat: add teaching sets

---
+++ src/Wrappers/TeachingSet.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace spkm\isams\Wrappers;
+
+use spkm\isams\Wrapper;
+
+/**
+ * Wrapper for the array returned by the iSAMS REST API endpoint.
+ */
+class TeachingSet extends Wrapper
+{
+    /*
+     * @var bool
+     */
+    protected $isHidden;
+
+    /**
+     * Handle the data.
+     *
+     * @return void
+     */
+    protected function handle(): void
+    {
+        $this->isHidden = (bool) optional($this->item)->hidden;
+    }
+}
