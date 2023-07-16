221adcf1b4b07ba294443dac29705da2af2ddd8e
Fred Bradley <code@fredbradley.co.uk>
2023-07-16 12:33:37 +0100

    patch: custom exception for use cases when isams throws a 404

---
+++ src/Exceptions/IsamsInstanceNotFound.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace spkm\isams\Exceptions;
+
+class IsamsInstanceNotFound extends \Exception
+{
+ /**
+  * TODO: (See below)
+  * Currently offered to be used by the consuming project. When the exception is a 404.
+  * But in time, we aim to re-write this package so that this exception will be
+  * thrown by the package itself on each request made to the API.
+  */
+}
